c261ce25493076c4bc9ac8455fa7e1805ebdc5c7
Ashwini300794 <61134572+Ashwini300794@users.noreply.github.com>
2020-02-17 11:41:59 +0530

    Update Demo_GIT.java

--- DemoGIT/src/com/demo/Demo_GIT.java
+++ DemoGIT/src/com/demo/Demo_GIT.java
@@ -5,7 +5,7 @@ public class Demo_GIT {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to GIThub1");
-
+	System.out.println("Welcome to GIThub2");
 	}
 
 }
